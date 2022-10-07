df7a2b7a2712fc97642e836f44f05f1cd467d1b6
magulloff <przemczar2001@gmail.com>
2022-10-07 03:16:37 +0200

    delete working

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
 async function main() {
     const lib = await import("../pkg/index.js").catch(console.error);
     const canvas = document.getElementById("board");
-    const e = canvas.getContext('2d');
+    const radioEdit = document.getElementById("Edit");
+    const radioHl = document.getElementById("Highlight");
     const canvasRef = lib.Canvas.new(document);
 
     var elemLeft = canvas.offsetLeft + canvas.clientLeft;
     var elemTop = canvas.offsetTop + canvas.clientTop;
 
-    canvas.addEventListener('click', (event) => {
+    canvas.oncontextmenu = (event) => {
         var x = event.pageX - elemLeft,
             y = event.pageY - elemTop;
-        canvasRef.onclick(x,y);
-    });
+        event.preventDefault();
+        console.log('right');
+        canvasRef.on_right_click(x, y);
+    };
 
-    canvas.addEventListener('mousemove', (event) => {
-        e.clearRect(0,0,1000,700);
+    canvas.onclick = (event) => {
         var x = event.pageX - elemLeft,
             y = event.pageY - elemTop;
-        canvasRef.movemouse(x,y);
-    });
+        console.log('left',x,y);
+        canvasRef.on_left_click(x,y);
+    };
+
+    canvas.onmousemove = (event) => {
+        var x = event.pageX - elemLeft,
+            y = event.pageY - elemTop;
+        console.log(x,y);
+        canvasRef.on_move_mouse(x,y);
+    };
+
+    radioEdit.onclick = () => {
+        console.log('radion click');
+        canvasRef.set_edit_state();
+    };
+
+    radioHl.onclick = () => {
+        console.log('hl click');
+        canvasRef.set_highlight_state();
+    };
 }
 
 main();
